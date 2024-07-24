7d2f2e4ab4c5ed95c820004a095e7174e6e95f44
heajun jeong <heajun35@gmail.com>
2024-07-24 10:51:40 +0900

    [Feat] -side navigation bar 제작

---
+++ src/components/homeComponents/SideNavigationBar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const SideNavigationBar = () => {
+  return (
+    <nav>
+      <li>
+        <p>홈</p>
+      </li>
+      <li>
+        <p>랭킹</p>
+      </li>
+      <li>
+        <p>군장검사</p>
+      </li>
+      <li>
+        <p>수집품</p>
+      </li>
+      <li>
+        <p>길드</p>
+      </li>
+      <li>
+        <p>도구</p>
+      </li>
+      <li>
+        <p>빠른도구</p>
+      </li>
+    </nav>
+  );
+};
+
+export default SideNavigationBar;
