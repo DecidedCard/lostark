3288c4658b81b172709c64108233d3b3d5647aaf
heajun jeong <heajun35@gmail.com>
2024-07-24 10:58:30 +0900

    [Feat] - 메인페이지 중앙 정렬

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={`${inter.className} max-w-7xl bg-bg-100`}>
+      <body className={`${inter.className} mx-auto max-w-7xl bg-bg-100`}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import Input from "@/components/homeComponents/Input";
+import SideNavigationBar from "@/components/homeComponents/SideNavigationBar";
 
 export default function Home() {
   return (
-    <div>
-      <Input />
+    <div className="flex">
+      <SideNavigationBar />
+      <main>
+        <Input />
+      </main>
     </div>
   );
 }
