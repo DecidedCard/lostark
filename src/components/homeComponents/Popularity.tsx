70b73e213f18bb4b71ab9b867eb9588d514f859a
heajun jeong <heajun35@gmail.com>
2024-07-25 12:44:32 +0900

    [Feat] - 실시간 인기 컴포넌트 작성

--- src/components/homeComponents/Popularity.tsx
+++ src/components/homeComponents/Popularity.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Popularity = () => {
-  return <div>Popularity</div>;
+  return (
+    <section className="m-2 p-2 w-60 border border-solid border-primary-200 rounded-lg">
+      <h2>실시간 인기 캐릭터</h2>
+    </section>
+  );
 };
 
 export default Popularity;
