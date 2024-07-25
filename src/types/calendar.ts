f5128bd1683ebf8cb4ccbc914ed8565edebee5fe
heajun jeong <heajun35@gmail.com>
2024-07-26 00:00:52 +0900

    [Feat] - calrendar type 수정

--- src/types/calendar.ts
+++ src/types/calendar.ts
@@ -1,23 +1,21 @@
-type calendar = [
-  {
-    CategoryName: "string";
-    ContentsName: "string";
-    ContentsIcon: "string";
-    MinItemLevel: 0;
-    StartTimes: ["2024-07-25T13:19:32.910Z"];
-    Location: "string";
-    RewardItems: [
-      {
-        ItemLevel: 0;
-        Items: [
-          {
-            Name: "string";
-            Icon: "string";
-            Grade: "string";
-            StartTimes: [string];
-          }
-        ];
-      }
-    ];
-  }
-];
+type calendar = {
+  CategoryName: "string";
+  ContentsName: "string";
+  ContentsIcon: "string";
+  MinItemLevel: 0;
+  StartTimes: ["2024-07-25T13:19:32.910Z"];
+  Location: "string";
+  RewardItems: [
+    {
+      ItemLevel: 0;
+      Items: [
+        {
+          Name: "string";
+          Icon: "string";
+          Grade: "string";
+          StartTimes: [string];
+        }
+      ];
+    }
+  ];
+};
